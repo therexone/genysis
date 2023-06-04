a532e466646c0490deca25e7ac694b316f9c9e3b
therexone <ayushsingh871@gmail.com>
2023-06-04 21:10:34 +0530

    fix: pwa

--- next.config.js
+++ next.config.js
@@ -1,29 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-    runtimeCaching: [
-      ...runtimeCaching,
-      {
-        urlPattern: /\/api\/.*\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-        handler: "StaleWhileRevalidate",
-        options: {
-          cacheName: "static-image-asset-api",
-          expiration: {
-            maxEntries: 128,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  runtimeCaching: [
+    ...runtimeCaching,
+    {
+      urlPattern: /\/api\/.*\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "static-image-asset-api",
+        expiration: {
+          maxEntries: 128,
+          maxAgeSeconds: 24 * 60 * 60, // 24 hours
         },
       },
-    ],
-  },
-  ...nextConfig,
+    },
+  ],
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
 });
